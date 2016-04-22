09ebbcdc0f18e68e166f48c9a16e19852e204a9e
Pavel Kvas <kvaspasha@yandex.ua>
2016-04-22 19:58:20 +0300

    refactor CreateGrid()

--- Assets/scripts/GridManager.cs
+++ Assets/scripts/GridManager.cs
@@ -38,47 +38,51 @@ namespace WavingGrid
             {
                 for (int j = 0; j < numCols; j++)
                 {
-                    var cubePos = new Vector3(i * 1 + .5f, 0, j * 1 + 0.5f);
-                    Vector3 position = cubePos;
-                    GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    const float halfCubeWidth = .5f;
 
-                    cube1.GetComponent<Renderer>().material = cubeMat;
-                    var cubeTransform = cube1.transform;
+                    Vector3 cubePos = new Vector3(i * 1, 0, j * 1);
+                    cubePos += new Vector3(halfCubeWidth, 0, halfCubeWidth);
+
+                    Vector3 position = cubePos;
+
+                    GameObject cubeGO = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                    cubeGO.GetComponent<Renderer>().material = cubeMat;
+                    var cubeTransform = cubeGO.transform;
 
                     //move down by half cube height for scaling from one side
-                    position += new Vector3(0, -0.5f, 0);
+                    position += new Vector3(0, -halfCubeWidth, 0);
 
                     cubeTransform.position = position;
 
                     GameObject go = new GameObject("Scaler object");
                     go.transform.parent = transform;
-                    cube1.transform.parent = go.transform;
+                    cubeGO.transform.parent = go.transform;
 
 
-                    var rb = cube1.AddComponent<Rigidbody>();
+                    var rb = cubeGO.AddComponent<Rigidbody>();
                     rb.useGravity = false;
                     rb.constraints = RigidbodyConstraints.FreezeRotation;
 
-                    cube1.GetComponent<Collider>().enabled = false;
+                    cubeGO.GetComponent<Collider>().enabled = false;
                     go.transform.localScale = new Vector3(1, MaxDisplacement, 1);
 
-                    cube1.AddComponent<SpringJoint>().spring = SpringBase;
+                    cubeGO.AddComponent<SpringJoint>().spring = SpringBase;
 
-                    cube1.AddComponent<CubeMovement>();
-                    GameObject cube = cube1;
+                    cubeGO.AddComponent<CubeMovement>();
 
-                    cubesGrid[i, j] = cube;
+                    cubesGrid[i, j] = cubeGO;
 
                     //create quad collider for detecting pressure
                     var quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-
+                    
                     PressureDetector pressureDetector = quad.AddComponent<PressureDetector>();
-                    pressureDetector.Init(cube, MaxDisplacement);
-                    cube.GetComponent<CubeMovement>().initialY = pressureDetector.initY;
+                    pressureDetector.Init(cubeGO, MaxDisplacement);
+                    cubeGO.GetComponent<CubeMovement>().initialY = pressureDetector.initY;
 
                     quad.transform.position = cubePos;
                     quad.transform.Rotate(90,0,0);
-                    quad.transform.parent = cube.transform.parent;
+                    quad.transform.parent = cubeGO.transform.parent;
                     quad.GetComponent<Renderer>().enabled = false;
                 }
             }
