e07cad69b293bf29d44b570abebe78a36f01339b
Pavel Kvas <kvaspasha@yandex.ua>
2016-04-25 18:52:50 +0300

    add multitouch support

--- Assets/scripts/InputManager.cs
+++ Assets/scripts/InputManager.cs
@@ -1,23 +1,50 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace WavingGrid
 {
     public class InputManager : MonoBehaviour
     {
         public GridManager GridManager;
-
+        private Dictionary<int, PressureDetector> activeDetectors = new Dictionary<int, PressureDetector>();
+         
         void Update()
         {
             if (Input.touchSupported)
             {
-                if (Input.touchCount > 0)
+                foreach (var touch in Input.touches)
                 {
-                    Touch touch = Input.GetTouch(0);
-                    if (touch.phase == TouchPhase.Ended)
+                    PressureDetector detector;
+                    switch (touch.phase)
                     {
-                        GridManager.DisableInteractive();
+                        case TouchPhase.Ended:
+                            detector = activeDetectors[touch.fingerId];
+                            detector.SendMessage("OnMouseExit");
+                            activeDetectors.Remove(touch.fingerId);
+                            break;
+                        case TouchPhase.Began:
+                            var ray = Camera.main.ScreenPointToRay(touch.position);
+
+                            RaycastHit hit;
+                            if (Physics.Raycast(ray, out hit, 100))
+                            {
+                                detector = hit.collider.GetComponent<PressureDetector>();
+
+                                hit.collider.SendMessage("OnMouseEnter");
+                                activeDetectors.Add(touch.fingerId, detector);
+                            }
+                            break;
                     }
+                    
                 }
+
+                if (activeDetectors.Count == 0)
+                {
+                    if(GridManager.isInteractive) GridManager.DisableInteractive();
+                }
+
+
+                //GridManager.DisableInteractive();
             }
             else
             {
