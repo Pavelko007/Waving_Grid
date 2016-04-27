b4141c2035858d9d0c7407b71d2e938337f78218
Pavel Kvas <kvaspasha@yandex.ua>
2016-04-27 13:01:21 +0300

    add scene for testing multitouch

---
+++ Assets/touchDisplay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class touchDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    Debug.Log(Camera.allCamerasCount);
+	}
+	
+	// Update is called once per frame
+	void OnDrawGizmos () {
+        foreach (var touch in Input.touches)
+        {
+            Debug.Log(Input.touchCount);
+
+
+            Vector3 position = touch.position;
+            
+            //var screenToWorldPoint = Camera.main.ScreenToWorldPoint(position);
+            var spherePos = Camera.main.ScreenPointToRay(position).GetPoint(10);
+            
+            Gizmos.DrawSphere(spherePos, 1);
+        }
+    }
+}
