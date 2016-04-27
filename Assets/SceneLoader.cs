a92f77171afc48e60c8091867090701a3907dd44
Pavel Kvas <kvaspasha@yandex.ua>
2016-04-27 13:34:42 +0300

    add a button for jumping between scenes

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNext(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
